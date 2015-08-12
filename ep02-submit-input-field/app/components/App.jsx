d5a72fe3a1c5a6fb49761573c175477a150f3d6f
Neeraj Singh <neeraj@bigbinary.com>
2015-08-12 16:12:40 -0400

    added button

--- ep02-submit-input-field/app/components/App.jsx
+++ ep02-submit-input-field/app/components/App.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default class App extends React.Component {
   render () {
     return  <div>
-              TODO
+              <p> TODO </p>
+              <button> Submit </button>
             </div>;
   }
 }
