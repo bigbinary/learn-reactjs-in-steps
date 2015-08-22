00ee9601b6d9c205847196ce7824c25b2ec0928e
Neeraj Singh <neeraj@bigbinary.com>
2015-08-21 22:37:52 -0400

    moved state from DisplayList component to DisplayItem component

--- ep08-displayitem-component/app/components/DisplayItem.jsx
+++ ep08-displayitem-component/app/components/DisplayItem.jsx
@@ -4,12 +4,29 @@ export default class DisplayItem extends React.Component {
 
   constructor () {
     super();
+    this.state = { done: false };
+  }
+
+  handleOnChange (event) {
+    var _done = !this.state.done;
+    this.setState({ done: _done });
   }
 
   render () {
-    return  <ul>
-              <li> This is display item </li>
-            </ul>;
+    var item = this.props.item;
+
+    return <li key={item}>
+              <input
+                checked={this.state.done}
+                onChange={this.handleOnChange.bind(this)}
+                type="checkbox"
+                style={{ fontSize: 'x-large' }} />
+
+              { item }
+              <a href='#' onClick={ this.props.handleDelete.bind(null, item) }>
+                [x]
+              </a>
+           </li>;
   }
 
 }
--- ep08-displayitem-component/app/components/DisplayList.jsx
+++ ep08-displayitem-component/app/components/DisplayList.jsx
@@ -3,36 +3,14 @@ import DisplayItem from './DisplayItem';
 
 export default class DisplayList extends React.Component {
 
-  constructor () {
-    super();
-    this.state = { done: false };
-  }
-
-  handleOnChange (event) {
-    var _done = !this.state.done;
-    this.setState({ done: _done });
-  }
-
   render () {
-    return  <ul>
-              { this.props.items.map((item, i) => {
-                return <li key={item}>
-                          <input
-                            checked={this.state.done}
-                            onChange={this.handleOnChange.bind(this)}
-                            type="checkbox"
-                            style={{ fontSize: 'x-large' }} />
-
-                          { item }
-                          <a href='#' onClick={ this.props.handleDelete.bind(null, item) }>
-                            [x]
-                          </a>
-                       </li>;
-              }) }
-
-              <DisplayItem />
-            </ul>;
+    return <ul>
+            { this.props.items.map((item, i) => {
+              return <DisplayItem
+                        item={item}
+                        handleDelete={this.props.handleDelete.bind(null, item)} />;
+            }) }
+           </ul>
   }
 
 }
-
