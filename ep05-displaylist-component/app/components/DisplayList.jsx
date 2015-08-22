1cff7c592fa835a98adb53f567fbe6a67f4b87d2
Neeraj Singh <neeraj@bigbinary.com>
2015-08-21 21:29:41 -0400

    remove warnign by adding key

--- ep05-displaylist-component/app/components/DisplayList.jsx
+++ ep05-displaylist-component/app/components/DisplayList.jsx
@@ -5,7 +5,7 @@ export default class DisplayList extends React.Component {
   render () {
     return  <ul>
               { this.props.items.map((item, i) => {
-                return <li> { item } </li>;
+                return <li key={item}> { item } </li>;
               }) }
             </ul>;
   }
