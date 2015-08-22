0a292a79d9c06791c016aa764e55b48fda3aa87b
Neeraj Singh <neeraj@bigbinary.com>
2015-08-21 21:48:21 -0400

    baseline for ep07

---
+++ ep07-mark-as-done/app/components/DisplayList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class DisplayList extends React.Component {
+
+  render () {
+    return  <ul>
+              { this.props.items.map((item, i) => {
+                return <li key={item}>
+                          { item }
+                          <a href='#' onClick={ this.props.handleDelete.bind(null, item) }>
+                            [x]
+                          </a>
+                       </li>;
+              }) }
+            </ul>;
+  }
+
+}
+
