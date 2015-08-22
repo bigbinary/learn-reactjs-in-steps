2fb83feee5d6b79fb9571bfb917a0d0302d0b2e8
Neeraj Singh <neeraj@bigbinary.com>
2015-08-21 21:32:37 -0400

    added delete link

--- ep06-delete-todo/app/components/DisplayList.jsx
+++ ep06-delete-todo/app/components/DisplayList.jsx
@@ -5,7 +5,10 @@ export default class DisplayList extends React.Component {
   render () {
     return  <ul>
               { this.props.items.map((item, i) => {
-                return <li key={item}> { item } </li>;
+                return <li key={item}>
+                          { item }
+                          <a href='#'>[x]</a>
+                       </li>;
               }) }
             </ul>;
   }
