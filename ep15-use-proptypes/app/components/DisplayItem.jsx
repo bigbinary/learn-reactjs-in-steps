a80c9255410d0de52426679996bba86e07b487cd
Neeraj Singh <neeraj@bigbinary.com>
2015-08-28 15:00:07 -0400

    baseline for episode 15

---
+++ ep15-use-proptypes/app/components/DisplayItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class DisplayItem extends React.Component {
+
+  render () {
+    var todo = this.props.todo;
+
+    return <li>
+              <input
+                checked={todo.done}
+                onChange={this.props.handleDone.bind(null, todo.id)}
+                type="checkbox"
+                style={{ fontSize: 'x-large' }} />
+
+              { todo.title }
+              <a href='#' onClick={ this.props.handleDelete.bind(null, todo.id) }>
+                [x]
+              </a>
+           </li>;
+  }
+
+}
