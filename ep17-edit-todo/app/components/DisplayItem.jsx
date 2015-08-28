d4637ec8e5ee230128a6fdd4b7066da10fdce837
Neeraj Singh <neeraj@bigbinary.com>
2015-08-28 15:37:24 -0400

    handleEditing and handleEditingDone

--- ep17-edit-todo/app/components/DisplayItem.jsx
+++ ep17-edit-todo/app/components/DisplayItem.jsx
@@ -7,6 +7,13 @@ export default class DisplayItem extends React.Component {
     this.state = { editing: false }
   }
 
+  handleEditing (event) {
+    this.setState({ editing: true });
+  }
+
+  handleEditingDone (event) {
+  }
+
   render () {
     var todo = this.props.todo;
 
@@ -20,7 +27,7 @@ export default class DisplayItem extends React.Component {
     }
 
     return <li className={ todo.done ? 'done' : '' }>
-              <div style={viewStyle}>
+              <div style={viewStyle} onDoubleClick={this.handleEditing.bind(this)}>
                 <input
                   checked={todo.done}
                   onChange={this.props.handleDone.bind(null, todo.id)}
@@ -39,6 +46,7 @@ export default class DisplayItem extends React.Component {
               </div>
 
               <input  type="text"
+                      onKeyDown={this.handleEditingDone.bind(this)}
                       style={editStyle}
                       value={todo.title} />
            </li>
