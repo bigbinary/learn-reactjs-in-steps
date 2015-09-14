dd756ca6833b99970819dadfd22e61c27d62d50d
Chirag Shah <shahchirag1709@gmail.com>
2015-09-14 18:21:09 +0530

    added production url and placeholder text for api key

--- ep19-call-api-jquery/app/utils/constants.js
+++ ep19-call-api-jquery/app/utils/constants.js
@@ -1,6 +1,6 @@
 var Constants = {
-  BASE_URL: "http://localhost:3000/api/v1/",
-  API_KEY: "93d2612b-20df-42e3-89fa-6af7889be7e5"
+  BASE_URL: "http://lrjis-api-production.herokuapp.com/api/v1/",
+  API_KEY: "add your api key here"
 };
 
 module.exports = Constants;
