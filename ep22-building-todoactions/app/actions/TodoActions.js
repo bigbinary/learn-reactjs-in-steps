44dd3b5539d754f00d26df5359ff964f7b2b036b
Neeraj Singh <neeraj@bigbinary.com>
2015-10-11 11:51:39 -0400

    TodoAction updated db using api

--- ep22-building-todoactions/app/actions/TodoActions.js
+++ ep22-building-todoactions/app/actions/TodoActions.js
@@ -1,11 +1,17 @@
+var api = require("../utils/api");
+
 var TodoActions = {
 
   markTodoDone: (todo) => {
     console.log("Marking TODO as done");
+    api.markTodoDone(todo)
+       .then( () => { console.log("marked TODO as done successfully"); })
   },
 
   markTodoUnDone: (todo) => {
     console.log("Marking TODO as undone");
+    api.markTodoUnDone(todo)
+       .then( () => { console.log("marked TODO as undone successfully"); })
   }
 
 }
